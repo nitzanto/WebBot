74f01a4a69fa3f71204c0c1be5b324bb4cdf0de2
Nitzan <nitzan16001@gmail.com>
2023-08-11 19:35:53 +0300

    Connected main with bookjng class

--- booking/booking.py
+++ booking/booking.py
@@ -1,10 +1,12 @@
 import booking.constants as const
+import os
 from selenium import webdriver
 
 
 class Booking(webdriver.Chrome):
     def __init__(self, driver_path=r"C:\SeleniumDrivers"):
         self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
 
     def land_first_page(self):
---
+++ main.py
@@ -0,0 +1,4 @@
+from booking.booking import Booking
+
+instance = Booking()
+instance.land_first_page()
